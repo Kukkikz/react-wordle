b79630c1dd7099172ee3073f092b4584ebd286e7
Pranot <pranout_kik@hotmail.com>
2022-05-03 17:07:31 +0700

    create react-wordle

---
+++ src/Words.js
@@ -0,0 +1,24 @@
+import wordBank from "./wordle-bank.txt";
+
+export const boardDefault = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+export const generateWordSet = async () => {
+  let wordSet;
+  let todayWord;
+  await fetch(wordBank)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArr = result.split("\r\n");
+      todayWord = wordArr[Math.floor(Math.random() * wordArr.length)]
+      wordSet = new Set(wordArr);
+      console.log(todayWord);
+    });
+  return { wordSet, todayWord };
+};
